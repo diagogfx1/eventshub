244378f9abc0a731cf0d666aa09b02591f982e39
diagogfx1 <info@ghupload.com>
2024-01-06 19:04:04 +0000

    Add ENV

--- eventshub/settings.py
+++ eventshub/settings.py
@@ -21,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ki#-rkyx+ba@e52@l^e20oc%kv%yy$dyb&gb&$1e!8&^@o8dct'
+# SECRET_KEY = 'django-insecure-ki#-rkyx+ba@e52@l^e20oc%kv%yy$dyb&gb&$1e!8&^@o8dct'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-ki#-rkyx+ba@e52@l^e20oc%kv%yy$dyb&gb&$1e!8&^@o8dct')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
